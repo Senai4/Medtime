06068e4838f8999c9976427e1bef5fefdf37b840
Senai4 <senaisp5@gmail.com>
2025-11-07 23:40:06 -0300

    07.11.2025

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,52 +1,69 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  menuativo = false;
-  isLoggedIn = false;
-  userName = '';
-  isAdmin = false;
+export class HomeComponent  implements OnInit, OnDestroy{
+  constructor(private router: Router) {}
 
-  @ViewChild('menu') menuElement!: ElementRef;
-  @ViewChild('menuIcon') menuIcon!: ElementRef;
+  irParaLogin(): void {
+    this.router.navigate(['/login']);
+  }
 
-  constructor(
-    public authService: AuthService) {}
+  irParaSobreNos(): void {
+    this.router.navigate(['/sobre']);
+  }
+
+  irParaCuriosidades(): void {
+    this.router.navigate(['/curiosidades']);
+  }
+
+  // Lista de imagens
+  images = [
+    'assets/img/carrossel/1.svg',
+    'assets/img/carrossel/2.svg',
+    'assets/img/carrossel/3.svg',
+    'assets/img/carrossel/4.svg',
+    'assets/img/carrossel/5.svg',
+    'assets/img/carrossel/6.svg',
+  ];
+
+  // Estado do Carrossel
+  currentIndex = 0;
+  private slideInterval: any;
 
   ngOnInit(): void {
-    // Observa mudanças de login
-    this.authService.currentUser$.subscribe((user: { nome: string; perfil: string; }) => {
-      this.isLoggedIn = !!user;
-      if (this.isLoggedIn) {
-        this.userName = user.nome;
-        this.isAdmin = user.perfil === 'admin';
-      } else {
-        this.userName = '';
-        this.isAdmin = false;
-      }
-    });
+    this.startAutoSlide();
   }
 
-  toggleMenu() {
-    this.menuativo = !this.menuativo;
+  ngOnDestroy(): void {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
   }
 
-  @HostListener('document:click', ['$event'])
-  clickFora(event: MouseEvent) {
-    const target = event.target as HTMLElement;
-    if (
-      this.menuElement &&
-      this.menuIcon &&
-      !this.menuElement.nativeElement.contains(target) &&
-      !this.menuIcon.nativeElement.contains(target)
-    ) {
-      this.menuativo = false;
+  startAutoSlide(): void {
+    this.slideInterval = setInterval(() => {
+      this.moveSlide(1);
+    }, 3000);
+  }
+
+  pauseSlide(): void {
+    clearInterval(this.slideInterval);
+  }
+
+  moveSlide(direction: number): void {
+    const newIndex = this.currentIndex + direction;
+
+    if (newIndex < 0) {
+      this.currentIndex = this.images.length - 1;
+    } else if (newIndex >= this.images.length) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex = newIndex;
     }
   }
 }
-
